6a7da4f6a7ff33c38d189207858d2d9fd2096e17
Mateusz Krasny <mkrasny@opidomain.local>
2020-02-10 11:50:27 +0100

    persistenceContex=extended

--- src/main/java/application/dao/UserDAOImpl.java
+++ src/main/java/application/dao/UserDAOImpl.java
@@ -3,16 +3,16 @@ package application.dao;
 import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
 import javax.transaction.Transactional;
 
 @Transactional
 @Repository
 public class UserDAOImpl implements UserDAO {
 
-    @PersistenceContext
+    @PersistenceContext(type= PersistenceContextType.EXTENDED)
     private EntityManager em;
 
-    @Override
     public void merge(Object object) {
         this.em.merge(object);
     }
